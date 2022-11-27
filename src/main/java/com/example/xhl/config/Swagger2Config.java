682dd86891057ccbe6962d9a804dbcc9e95fc75f
xhljack <56154079+xhljack@users.noreply.github.com>
2022-11-27 20:32:32 +0800

    图片无法加载
    Signed-off-by: xhljack <56154079+xhljack@users.noreply.github.com>

--- src/main/java/com/example/xhl/config/Swagger2Config.java
+++ src/main/java/com/example/xhl/config/Swagger2Config.java
@@ -73,7 +73,7 @@ public class Swagger2Config {
                 .title("网站-API文档")
                 .description("本文档描述了网站微服务接口定义")
                 .version("1.0")
-                .contact(new Contact("atguigu", "http://atguigu.com", "512111559@qq.com"))
+                .contact(new Contact("xhl", "http://atguigu.com", "2253265923@qq.com"))
                 .build();
     }
 
@@ -83,7 +83,7 @@ public class Swagger2Config {
                 .title("后台管理系统-API文档")
                 .description("本文档描述了后台管理系统微服务接口定义")
                 .version("1.0")
-                .contact(new Contact("atguigu", "http://atguigu.com", "512111559@qq.com"))
+                .contact(new Contact("xhl", "http://atguigu.com", "2253265923@qq.com"))
                 .build();
     }
 
